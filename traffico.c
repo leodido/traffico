96944786eb6594b49a0c0a210c46e4c6e3841786
Leonardo Di Donato <leodidonato@gmail.com>
2022-05-26 23:34:36 +0200

    refactor: make the CLI use the API
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- traffico.c
+++ traffico.c
@@ -6,7 +6,7 @@
 #include <argp.h>
 #include <bpf/libbpf.h>
 
-#include "rfc3330.skel.h"
+#include "api.h"
 
 #define CLI_NAME "traffico"
 const char *argp_program_version = CLI_NAME " 0.0";
@@ -34,14 +34,6 @@ const struct argp_option argp_opts[] = {
     {0}                           // .
 
 };
-
-struct args
-{
-    bool verbose;
-    char ifname[IF_NAMESIZE];
-    int ifindex;
-    enum bpf_tc_attach_point attach_point;
-} config;
 
 static error_t parse_arg(int key, char *arg, struct argp_state *state)
 {
@@ -158,11 +150,39 @@ void sig_handler(int signo)
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
-    if (level == LIBBPF_DEBUG && !config.verbose)
+    if (level == LIBBPF_DEBUG) /* && !config.verbose)*/
     {
         return 0;
     }
     return vfprintf(stderr, format, args);
+}
+
+int run(struct bpf_tc_hook hook, struct bpf_tc_opts opts)
+{
+    // Block until user signal
+    while (!g_stop)
+    {
+        fprintf(stderr, ".");
+        sleep(1);
+    }
+    fprintf(stderr, "\n");
+
+    // Detach the TC hook
+    int err;
+    char buf[100];
+    buf[sizeof(buf) - 1] = '\0';
+
+    opts.prog_fd = opts.prog_id = 0;
+    opts.flags = 0;
+    err = bpf_tc_detach(&hook, &opts);
+    if (err)
+    {
+        libbpf_strerror(err, buf, sizeof(buf));
+        fprintf(stderr, "traffico: fail: detaching the TC eBPF program: %s\n", buf);
+    }
+    fprintf(stdout, "traffico: done: detaching the TC eBPF program\n");
+
+    return err < 0 ? -err : err;
 }
 
 int main(int argc, char **argv)
@@ -178,103 +198,19 @@ int main(int argc, char **argv)
     err = argp_parse(&argp, argc, argv, ARGP_IN_ORDER, NULL, &config);
     if (err)
     {
-        return err;
+        return 1;
     }
-
-    fprintf(stdout, "traffico: verbose? %d\n", config.verbose);
-    fprintf(stdout, "traffico: using interface %d: %s\n", config.ifindex, config.ifname);
-    fprintf(stdout, "traffico: attaching at %d\n", config.attach_point);
-
-    exit(0);
 
     // Setup signal handling
     if (signal(SIGINT, sig_handler) == SIG_ERR || signal(SIGTERM, sig_handler) == SIG_ERR)
     {
         fprintf(stderr, "traffico: can't handle signal: %s\n", strerror(errno));
-        goto cleanup;
+        return 1;
     }
 
     // Setup libbpf
-    libbpf_set_strict_mode(LIBBPF_STRICT_ALL | LIBBPF_STRICT_AUTO_RLIMIT_MEMLOCK);
+    libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
     libbpf_set_print(libbpf_print_fn);
 
-    // Skeleton
-    struct rfc3330_bpf *obj = NULL;
-    obj = rfc3330_bpf__open();
-    if (!obj)
-    {
-        fprintf(stderr, "traffico: failed to open the eBPF skeleton\n");
-        return 1; // nothing to cleanup
-    }
-
-    err = rfc3330_bpf__load(obj);
-    if (err)
-    {
-        fprintf(stderr, "traffico: failed to load the eBPF skeleton\n");
-        goto cleanup;
-    }
-
-    DECLARE_LIBBPF_OPTS(bpf_tc_hook, hook, .ifindex = config.ifindex, .attach_point = config.attach_point);
-    err = bpf_tc_hook_create(&hook);
-    if (err)
-    {
-        // Moving on in case the hook file already exists // TODO > make configurable from arguments
-        if (err != -17)
-        {
-            libbpf_strerror(err, buf, sizeof(buf));
-            fprintf(stderr, "traffico: failed to create the qdisc: %s\n", buf);
-            goto cleanup;
-        }
-        fprintf(stdout, "traffico: hook already existing, trying to re-use it\n");
-    }
-
-    // Attach the TC eBPF program to the qdisc
-    int fd = bpf_program__fd(obj->progs.rfc3330); // TODO > make configurable from arguments
-    DECLARE_LIBBPF_OPTS(bpf_tc_opts, opts, .prog_fd = fd, .flags = BPF_TC_F_REPLACE);
-    err = bpf_tc_attach(&hook, &opts);
-    if (err)
-    {
-        libbpf_strerror(err, buf, sizeof(buf));
-        fprintf(stderr, "traffico: failed to attach the TC eBPF program: %s\n", buf);
-        goto cleanup;
-    }
-    fprintf(stdout, "traffico: filter handle: 0x%x\n", opts.handle);
-    fprintf(stdout, "traffico: filter priority: %d\n", opts.priority);
-    fprintf(stdout, "traffico: filter program ID: %d\n", opts.prog_id);
-
-    while (!g_stop)
-    {
-        fprintf(stderr, ".");
-        sleep(1);
-    }
-
-    int detach_err;
-    int destroy_err;
-
-    opts.prog_fd = opts.prog_id = 0;
-    opts.flags = 0;
-    detach_err = bpf_tc_detach(&hook, &opts);
-    if (detach_err)
-    {
-        libbpf_strerror(detach_err, buf, sizeof(buf));
-        fprintf(stderr, "traffico: errors detaching the TC eBPF program: %s\n", buf);
-        err = detach_err;
-    }
-    fprintf(stdout, "traffico: success detaching the TC eBPF program\n");
-
-cleanup:
-
-    hook.attach_point |= BPF_TC_INGRESS; // force the cleanup of the qdisc as well
-    destroy_err = bpf_tc_hook_destroy(&hook);
-    if (destroy_err)
-    {
-        libbpf_strerror(destroy_err, buf, sizeof(buf));
-        fprintf(stderr, "traffico: error destroying the eBPF hook: %s\n", buf);
-        err = destroy_err;
-    }
-    fprintf(stdout, "traffico: success destroying the eBPF hook\n");
-
-    rfc3330_bpf__destroy(obj);
-
-    return err < 0 ? -err : err;
+    return attach_rfc3330(&config, &run);
 }
