89a21ffab3239746f462879f54418d0e96b0bef3
Lorenzo Fontana <fontanalorenz@gmail.com>
2022-05-26 10:53:17 +0000

    fix: check real size instead of struct size
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>

--- bpf/rfc3330.bpf.c
+++ bpf/rfc3330.bpf.c
@@ -10,8 +10,9 @@ int rfc3330(struct __sk_buff *skb)
 {
     void *data_end = (void *)(unsigned long long)skb->data_end;
     void *data = (void *)(unsigned long long)skb->data;
-    const int l3_offset = sizeof(struct ethhdr);
-    const int l4_offset = l3_offset + sizeof(struct iphdr);
+
+    struct ethhdr *eth = data;
+    const int l3_offset = sizeof(*eth);
 
     if (data + l3_offset > data_end)
     {
@@ -19,7 +20,6 @@ int rfc3330(struct __sk_buff *skb)
         return TC_ACT_OK;
     }
 
-    struct ethhdr *eth = data;
     if (eth->h_proto != bpf_htons(ETH_P_IP))
     {
         bpf_printk("classifier: [eth] protocol is %d: continue", eth->h_proto);
@@ -27,6 +27,7 @@ int rfc3330(struct __sk_buff *skb)
     }
 
     struct iphdr *ip_header = data + l3_offset;
+    const int l4_offset = l3_offset + sizeof(*ip_header);
     if (data + l4_offset > data_end)
     {
         bpf_printk("classifier: [iph] size lenght check hit: continue");
@@ -39,11 +40,14 @@ int rfc3330(struct __sk_buff *skb)
         return TC_ACT_OK;
     }
 
+    bpf_printk("DADDR: %d", ip_header->daddr);
+    bpf_printk("SADDR: %d", ip_header->saddr);
+
     if (/*2.1.1.1*/ 33620225 == ip_header->daddr)
     {
         bpf_printk("classifier: [iph] destination address is 1.1.1.2 ...");
         bpf_printk("host byte order: %d", bpf_ntohl(ip_header->daddr));
-        return TC_ACT_OK;
+        return TC_ACT_SHOT;
     }
 
     if (172 == (ip_header->daddr >> 24 & 0xFF))
@@ -53,6 +57,7 @@ int rfc3330(struct __sk_buff *skb)
         {
             bpf_printk("classifier: [iph] destination address ends with 16.172 ...");
         }
+        return TC_ACT_SHOT;
     }
 
     return 0;
