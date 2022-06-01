5237fb5933dc69c1df2857bfb43c6d0d40e658e6
Leonardo Di Donato <leodidonato@gmail.com>
2022-06-01 12:41:06 +0200

    new(bpf): add block_ip program
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ bpf/block_port.bpf.c
@@ -0,0 +1,17 @@
+#include "vmlinux.h"
+#include "commons.bpf.h"
+
+#include <bpf/bpf_endian.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+// TODO > make this easy to configure via config struct
+const volatile __u32 input = 0;
+
+SEC("tc")
+int block_port(struct __sk_buff *skb)
+{
+    bpf_printk("TBD");
+
+    return TC_ACT_OK;
+}
